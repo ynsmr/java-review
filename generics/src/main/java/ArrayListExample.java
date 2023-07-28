5922b09b6cbcd4149c522c41986e02c162875ecd
yunusemre <yunusadil0@gmail.com>
2023-07-28 11:56:50 +0200

    Generics Review Started

--- generics/src/main/java/ArrayListExample.java
+++ generics/src/main/java/ArrayListExample.java
@@ -3,27 +3,17 @@ import java.util.ArrayList;
 public class ArrayListExample {
 
     public static void main(String[] args) {
-
-        ArrayList<Integer> items = new ArrayList<Integer>();
-
+        ArrayList items = new ArrayList();
         items.add(1);
         items.add(2);
         items.add(3);
         items.add(4);
-//        items.add("apple");
 
         printDouble(items);
 
     }
 
-    private static void printDouble(ArrayList<Integer> items) {
+    private static void printDouble(ArrayList items) {
 
-//        for(Object i : items){
-//            System.out.println((Integer)i * 2);
-//        }
-
-        for(int i : items){
-            System.out.println(i * 2);
-        }
     }
 }
--- generics/src/main/java/BaseballPlayer.java
+++
@@ -1,7 +0,0 @@
-public class BaseballPlayer extends Player{
-
-
-    public BaseballPlayer(String name) {
-        super(name);
-    }
-}

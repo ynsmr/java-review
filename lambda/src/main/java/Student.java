79a0f22191010f8b4d6b9231fae097f53ff79c74
yunusemre <yunusadil0@gmail.com>
2023-08-05 21:22:51 +0200

    Lombok Done

---
+++ lambda/src/main/java/Student.java
@@ -0,0 +1,22 @@
+import lombok.*;
+
+import java.util.Objects;
+//@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Data
+//@Getter
+//@Setter
+//@ToString
+//@EqualsAndHashCode
+//@RequiredArgsConstructor
+public class Student {
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String email;
+    private String password;
+    private String confirmPassword;
+    private final int age;
+}
+
+

27c192d34acdaa274fe877d8d3c653334bd49bd1
yunusemre <yunusadil0@gmail.com>
2023-08-15 18:08:56 +0200

    Streams-2

--- streams/src/main/java/DishTask.java
+++ streams/src/main/java/DishTask.java
@@ -1,6 +1,8 @@
 import java.util.Arrays;
 import java.util.List;
 
+import static java.util.Comparator.comparing;
+
 public class DishTask {
     public static void main(String[] args) {
         //TASK-2
@@ -24,7 +26,7 @@ public class DishTask {
 
         menu.stream()
                 .filter(i -> i.getCalories()<400)
-                .map(i -> i.getName())
+                .map(Dish::getName)
                 .forEach(System.out::println);
 
         System.out.println("-------------------------------------------");
@@ -34,6 +36,46 @@ public class DishTask {
         menu.stream()
                 .map(dish -> dish.getName().length())
                 .forEach(System.out::println);
+
+        System.out.println("----------------------------------");
+
+        menu.stream()
+                .map(Dish::getName)
+                .map(String::length)
+                .forEach(System.out::println);
+
+
+        //Print Three High Caloric Dish Name(>300)
+        System.out.println("------------------------------------------");
+        menu.stream()
+                .filter(c -> c.getCalories()>300)
+                .limit(3)
+                .map(Dish::getName)
+                .forEach(System.out::println);
+
+        //print all dish name that are below 400 calories in sorted
+        System.out.println("------------------------------");
+
+        menu.stream()
+                .filter(dish -> dish.getCalories()<400)
+                .map(Dish::getName)
+                .sorted()
+                .forEach(System.out::println);
+
+        System.out.println("----------------------------------");
+
+        menu.stream()
+                .filter(dish -> dish.getCalories()<400)
+                .sorted(comparing(Dish::getCalories).reversed())
+                .map(Dish::getName)
+                .forEach(System.out::println);
+
+
+
+
+
+
+
 
 
 

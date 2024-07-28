e0e3070e68547f6510de5e92d08ba20e240ca70e
yunusemre <yunusadil0@gmail.com>
2024-07-28 11:04:36 +0200

    Stream

--- streams/src/main/java/Course.java
+++ streams/src/main/java/Course.java
@@ -4,6 +4,10 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class Course {
-    private String courseName;
+
+    private String userName;
     private Integer courseId;
+
+
+
 }
--- streams/src/main/java/CreatingStream.java
+++ streams/src/main/java/CreatingStream.java
@@ -3,28 +3,31 @@ import java.util.List;
 import java.util.stream.Stream;
 
 public class CreatingStream {
+
     public static void main(String[] args) {
 
-        //Creating Stream from Array
+        //Creating Stream from array
         String[] courses = {"Java", "JS", "TS"};
         Stream<String> courseStream = Arrays.stream(courses);
 
         //Creating stream from collection
-        List<String> courseList = Arrays.asList("Java", "DS", "Spring");
-        Stream<String> courseStream2 = courseList.stream();
+        List<String> courseList = Arrays.asList("Java", "JS", "TS");
+        Stream<String> coursestream2 = courseList.stream();
 
         List<Course> myCourses = Arrays.asList(
                 new Course("Java", 100),
-                new Course("DS", 101),
-                new Course("Spring", 102),
-                new Course("Microservices", 103)
+                new Course("Spring", 101),
+                new Course("JS", 103),
+                new Course("MicroServices", 104)
         );
 
         Stream<Course> myCourseStream = myCourses.stream();
 
 
-        //Creating Stream from values
-        Stream<Integer> stream = Stream.of(1,2,3,4);
+        //Creating stream from values
+        Stream<Integer> streamValues = Stream.of(1, 2, 3, 4);
+
+
 
 
 

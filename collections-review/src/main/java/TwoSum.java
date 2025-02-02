612f8dde685d63ac4fc3c85cda91a7dc1c7d24b7
yunusemre <yunusadil0@gmail.com>
2025-02-02 17:46:57 +0100

    Map Review 2

--- collections-review/src/main/java/TwoSum.java
+++ collections-review/src/main/java/TwoSum.java
@@ -6,44 +6,50 @@ public class TwoSum {
     public static void main(String[] args) {
 
         int[] myArray = {2,7,9,11};
-        System.out.println(Arrays.toString(twoSum1(myArray, 9)));
+        System.out.println(Arrays.toString(twoSum(myArray, 9)));
+        System.out.println("------------------Optimal Solution----------------");
         System.out.println(Arrays.toString(twoSum2(myArray, 9)));
 
-    }
-    public static int[] twoSum1(int[] array, int target){
 
-        for (int i = 0; i < array.length-1 ; i++) {
-            for (int j = i+1; j <array.length; j++) {
+
+    }
+
+    public static int[] twoSum(int[] array, int target) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i+1; j < array.length; j++) {
                 if (array[i] + array[j] == target){
-                    return new int[]{i,j};
+                    return new int[]{i, j};
                 }
             }
         }
-        return new  int[]{};
+        return new int[]{};
     }
 
     //Optimal Solution
     public static int[] twoSum2(int[] array, int target){
-        //Create HashMap
-        //Iterate over the array.
-        //If a potential match exists, return that indices, else put the array value and index to map
-
+        //Create hashmap
         Map<Integer, Integer> map = new HashMap<>();
-
-        for (int i = 0; i<array.length; i++){
-            int potentialMatch = target-array[i];
+        
+        //Iterate over the array
+        for (int i = 0; i < array.length; i++) {
+            int potentialMatch = target - array[i];
             if (map.containsKey(potentialMatch)){
                 return new int[]{i, map.get(potentialMatch)};
+
             }else {
                 map.put(array[i], i);
+
             }
+            
         }
+        
+        //if a potential match exists return the indices, else put the array value and index to map
 
         return new int[]{};
-
     }
+
 }
 
-// n*n operation, if one pass is possible?
+// n*n operations, if one pass is possible?
 
 

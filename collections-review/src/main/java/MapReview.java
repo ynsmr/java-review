bc3f759d90ae1afedefc1f49ff07d24d24f23afa
yunusemre <yunusadil0@gmail.com>
2025-02-02 16:44:59 +0100

    Map Review

--- collections-review/src/main/java/MapReview.java
+++ collections-review/src/main/java/MapReview.java
@@ -13,21 +13,22 @@ public class MapReview {
 
     public static Character findFirstNonRepeating(String str){
         Map<Character, Integer> map = new HashMap<>();
-        //For counting chars
         int count;
-        //Counting chars and putting value in hashmap
+
         for (Character ch: str.toCharArray()){
-            if(map.containsKey(ch)){
-                count = map.get(ch);
+            if (map.containsKey(ch)){
+                count= map.get(ch);
                 map.put(ch, count+1);
+
             }else {
-                map.put(ch,1);
+                map.put(ch, 1);
             }
         }
 
-        // Starting from string, check if the count equals 1
         for (Character ch: str.toCharArray()){
-            if(map.get(ch)==1) return ch;
+            if (map.get(ch) == 1){
+                return ch;
+            }
         }
 
         return Character.MIN_VALUE;
